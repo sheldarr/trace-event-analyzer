9439d09ff2fec0c9db232050ebac7c81eea09ad4
sheldar <kewin.polok@gmail.com>
2017-10-14 12:47:50 +0200

    Handling multiple immediate events to analyse

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ import TraceEventsLoader from './src/trace-events-loader';
 import UniqueEventsAnalyzer from './src/unique-events-analyzer';
 
 const DEFAULT_DECIMAL_PLACES = 4;
+const DEFAULT_SEPARATOR = ',';
 
-const args = ArgumentsParser.parse(process.argv);
+const args = ArgumentsParser.parse(process.argv.slice(2));
 const events = TraceEventsLoader.load(args.path);
 
 winston.info(`${chalk.green('Provided args')} ${JSON.stringify(args, null, '\t')}`);
@@ -21,15 +22,16 @@ if (uniqueEvents) {
     winston.info(JSON.stringify(analysisSummary, null, '\t'));
 }
 
-const immediateEvent = args['immediate-event'];
 const decimalPlaces = args['decimal-places'] || DEFAULT_DECIMAL_PLACES;
-if (immediateEvent) {
-    const analysisSummary = ImmediateEventsAnalyzer.analyze(events, immediateEvent);
+const immediateEvents = args['immediate-events'].replace(' ', '').split(DEFAULT_SEPARATOR);
 
-    if(analysisSummary) {
-        winston.info(JSON.stringify({
-            deltas: StatisticsGenerator.generate(analysisSummary.deltas, decimalPlaces),
-            eps: StatisticsGenerator.generate(analysisSummary.eps, decimalPlaces)
-        }, null, '\t'));
-    }
-}
+immediateEvents.forEach((immediateEvent) => {
+    const analysisSummary = ImmediateEventsAnalyzer.analyze(events, immediateEvent);
+    
+        if(analysisSummary) {
+            winston.info(JSON.stringify({
+                deltas: StatisticsGenerator.generate(analysisSummary.deltas, decimalPlaces),
+                eps: StatisticsGenerator.generate(analysisSummary.eps, decimalPlaces)
+            }, null, '\t'));
+        }
+});
