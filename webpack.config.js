60088a541d241c50b915583a6b2bfecf3c005def
sheldar <kewin.polok@gmail.com>
2017-10-14 08:47:39 +0200

    Added webpack.config.js configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const DEFAULT_VIRTUAL_MACHINES = 20;
+
+module.exports = {
+    resolve: {
+        extensions: ['.js']
+    },
+    entry: {
+        main: './index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    plugins: [],
+    devtool: 'eval'
+}
